b9ad76b06711b03b58cd6ec2edd4722799c07a5c
jmolesco <jmolesco@yahoo.com>
2024-12-10 10:21:46 +0800

    Fix

---
+++ ShijiGroup/Models/SequenceContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShijiGroup.Models
+{
+    public class SequenceContext : DbContext
+    {
+        public SequenceContext(DbContextOptions<SequenceContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Sequence> Sequences { get; set; }
+      
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
+    }
+}
